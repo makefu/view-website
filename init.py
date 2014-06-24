f62601b09882efdd273ee2cbf6625734cf1e316a
makefu <root@pigstarter.de>
2014-06-24 14:54:16 +0200

    stuffies

--- init.py
+++ init.py
@@ -10,19 +10,25 @@ x1=0
 y1=0
 x2=1280
 y2=1280
+timeout=None
 def ghostCapture(screen_url,web_timeout=20):
-
-    gh = Ghost(display=":99",wait_timeout=int(web_timeout),viewport_size=(x2,y2), ignore_ssl_errors=True,log_level=logging.FATAL)
-    ghost_page,resources= gh.open(screen_url, auth=('none', 'none'))
-
+    import time
+    gh = Ghost(display=":99",wait_timeout=int(web_timeout),
+               viewport_size=(x2,y2), ignore_ssl_errors=True,
+               log_level=logging.FATAL)
+               
+    ghost_page,resources= gh.open(screen_url, 
+                auth=('none', 'none'))
+    
     img_path=mkstemp(suffix=".png")[1]
     gh.set_viewport_size(x2,y2)
+    time.sleep(timeout)
     gh.capture_to(img_path,region=(x1,y1,x2,y2))
 
     return img_path
 
 def usage(prog):
-    print("usage: %s uri"%prog)
+    print("usage: visit-page uri [timeout]")
 
 if __name__ == "__main__":
     import sys,os
@@ -34,10 +40,20 @@ if __name__ == "__main__":
             url="http://"+url
         host=urlparse(url).netloc
     except Exception,e:
-        print(e)
         usage(sys.argv[0])
         sys.exit(1)
-    print("Capturing '%s'"%url)
+    try:
+        timeout=int(sys.argv[2])
+        if not( 0 < timeout < 60):
+            sys.stdout.write("timeout must be between 0 and 60 seconds\n")
+            usage(sys.argv[0])
+            sys.exit(1)
+        sys.stdout.write("Timeout set to %d seconds\n"%timeout)
+    except:
+        timeout=0
+
+    sys.stdout.write("Capturing '%s'\n"%url)
+    sys.stdout.flush()
     image_path=ghostCapture(url)
     import pyimgur
     im = pyimgur.Imgur(CLIENT_ID)
